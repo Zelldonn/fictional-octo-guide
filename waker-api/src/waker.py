fa247b12fe6a24da854a7a65a7169a66df14fe4f
Pierre Stringari <pierre.stringari@sia-partners.com>
2022-05-01 17:20:04 +0200

    Updata gitignore, update api dockerfile and docker-compose,use ssh key to connect to host

--- waker-api/src/waker.py
+++ waker-api/src/waker.py
@@ -1,17 +1,22 @@
 import os
 import subprocess
 
-from typing import List
-
-from wakeonlan import send_magic_packet
+from operator import sub
 
 
-#4c:cc:6a:81:99:24 MSI
-def wake_up(mac_addresse : str):
+# 4c:cc:6a:81:99:24 MSI
+def wake_up(mac_address: str):
+    ssh_key_path = os.environ["SSH_PRIVATE_KEY_PATH"]
     user = os.environ["SSH_USER"]
     host = os.environ["SSH_HOST"]
-    ssh_key_path = os.environ["SSH_PRIVATE_KEY_PATH"]
-    cmd = f"ssh -i {ssh_key_path} {user}@{host} wakeonlan {mac_addresse}"
+    port = os.environ["SSH_PORT"]
+
+    cmd = f"ssh -i {ssh_key_path} {user}@{host} -p {port} wakeonlan {mac_address}"
     print(f"Sending : {cmd}")
-    subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    p = subprocess.Popen(
+        cmd,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
     return {"packet sent"}
